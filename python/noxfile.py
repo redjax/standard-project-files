8e99b918ac039b1c33b2901ce6cb5aaf2c91d3d6
redjax <redjax@noreply.localhost>
2023-12-26 13:54:52 -0500

    Update python/noxfile.py

--- python/noxfile.py
+++ python/noxfile.py
@@ -2,9 +2,17 @@ from __future__ import annotations
 
 import nox
 
-@nox.session(python=["3.11"], name="setup-env", reuse_venv=True)
-@nox.parametrize("pdm", ["2.11"])
-def setup_env(session: nox.Session, pdm: str):
+## List of Python versions for nox
+#  Note: Interpreters must already be available. Look into pyenv for installing
+#  multiple versions of Python at the same time
+PYTHON_VERSIONS: list[str] = ["3.11", "3.12"]
+
+## Default Python version to use throughout Nox sessions
+PYVER: str = "3.11"
+
+@nox.session(python=[PYVER], name="setup-env", reuse_venv=True)
+@nox.parametrize("pdm_ver", ["2.11"])
+def setup_env(session: nox.Session, pdm_ver: str):
     """Setup local dev environment with PDM.
     
     Description:
@@ -17,7 +25,7 @@ def setup_env(session: nox.Session, pdm: str):
     Setup the local development environment by installing dependencies & running whatever setup operations are needed.
     """
     ## Install with PDM
-    session.install(f"pdm>={pdm}")
+    session.install(f"pdm>={pdm_ver}")
 
     session.run("pdm", "install")
     session.run("pdm", "sync")
@@ -26,14 +34,14 @@ def setup_env(session: nox.Session, pdm: str):
     # session.run("pip", "install", "-r", "reequirements.txt")
 
 
-@nox.session(python=["3.11", "3.12"], name="tests", reuse_venv=True)
+@nox.session(python=PYTHON_VERSIONS, name="tests", reuse_venv=True)
 def tests(session: nox.Session):
     """Run Pytest suite."""
     session.install("pytest")
     session.run("pytest", "tests/")
 
 
-@nox.session(python=["3.11", "3.12"], name="lint", reuse_venv=True)
+@nox.session(python=[PYVER], name="lint", reuse_venv=True)
 def lint(session: nox.Session):
     """Lint & format code with Ruff, Black."""
     session.install("black", "ruff")
@@ -42,7 +50,7 @@ def lint(session: nox.Session):
     session.run("ruff", "check", "--config", "ruff.ci.toml", "src", "--fix")
 
 
-@nox.session(python=["3.11"], name="export", reuse_venv=True)
+@nox.session(python=[PYVER], name="export", reuse_venv=True)
 def export_requirements(session: nox.Session):
     """Export requirements.txt file(s) with PDM."""
     session.install("pdm")
